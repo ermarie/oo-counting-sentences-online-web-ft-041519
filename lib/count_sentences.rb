7fb52b77f760d075cbb42496d9f42ec07c1b878e
Erin Stalzer <erinstalzer@gmail.com>
2019-04-23 15:51:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -31,14 +31,12 @@ class String
     count = 0
     split_arr = []
     if /.?!/.match(self)
-      puts "match"
       split_arr = self.split(" ")
-      puts "arr: #{split_arr}"
       split_arr.each do |word|
         puts "each"
         if word.include?(".")
           puts "if"
-          count =+ 1
+          count += 1
           "count: #{count}"
         elsif word.include?("?")
           puts "elsif1"
